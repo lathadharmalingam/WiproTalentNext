2d04a4501721c121401fb475374f83cb18484e2a
lathadharmalingam <74287803+lathadharmalingam@users.noreply.github.com>
2022-05-23 18:51:56 +0530

    Update and rename problem 1 to problem1.java

---
+++ problem1.java
@@ -0,0 +1,9 @@
+//Write a Program that accepts two Strings as command line arguments and generate the output in the required format.
+
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println(args[0]+"Technologies"+args[1]);
+	}
+}
+
